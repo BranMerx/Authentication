71bc92406342589d0a220375fbd25bc3a2faa3ee
BranMerx <bmercadofranco@oakland.edu>
2024-10-23 19:39:02 -0400

    Starting Python and FPBioimage

---
+++ AuthenticatorProject.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("\Program Files\FPBioimage")
+
+
+def facial_collection():
+    pass
+
+def finger_collection():
+    pass
+
+
+def user_creation():
+    pass
+
+
+def face_validate():
+    pass
+
+def finger_validate():
+    pass
+
