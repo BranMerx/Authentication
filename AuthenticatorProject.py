5f2432360013acb97a15542e1b114b87a7fb7795
BranMerx <bmercadofranco@oakland.edu>
2024-10-28 15:24:14 -0400

    GUI Componenets and Buttons

--- AuthenticatorProject.py
+++ AuthenticatorProject.py
@@ -1,6 +1,24 @@
+#Libraries Imported
 import sys
-sys.path.append("\Program Files\FPBioimage")
+sys.path.append("C:\Program Files\FPBioimage\FPBioimage-4.5.0.exe")
 
+import cv2
+import numpy as np
+import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
+
+#Button and GUI Creation
+root = Tk()
+root.geometry('1000x1000')
+btn = Button (root, text = 'Register Profile', command = root.destroy)
+btn2 = Button (root, text = "Log in")
+btn3 = Button (root, text="Exit")
+btn.pack(side='top')
+btn2.pack(side='top')
+btn3.pack(side = 'top')
+
+root.mainloop()
 
 def facial_collection():
     pass
