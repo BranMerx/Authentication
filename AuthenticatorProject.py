7fd5dfe573961fa040d93ac76a43f0e7316e5f77
BranMerx <bmercadofranco@oakland.edu>
2024-11-10 23:03:43 -0500

    OpenCV development

--- AuthenticatorProject.py
+++ AuthenticatorProject.py
@@ -1,6 +1,4 @@
 #Libraries Imported
-import sys
-sys.path.append("C:\Program Files\FPBioimage\FPBioimage-4.5.0.exe")
 
 import cv2
 import numpy as np
@@ -11,20 +9,35 @@ import pyodbc
 from tkinter import *
 from tkinter.ttk import *
 
-
+face_casecade = cv2.CascadeClassifier(cv2.data.haarcascades + 'harcascade_frontalface_default.xml')
+finger_casecade = cv2.CascadeClassifier(cv2.data.haarcascades + 'harcascade_finger.xml')
 
 #Button and GUI Creation
 root = Tk()
 root.geometry('1000x1000')
-btn = Button (root, text = 'Register Profile', command = root.destroy)
-btn2 = Button (root, text = "Log in")
-btn3 = Button (root, text="Exit")
-btn.pack(side='top')
-btn2.pack(side='top')
-btn3.pack(side = 'top')
+btn = Button (root, text = 'Register Profile', command=facial_collection)
+btn2 = Button (root, text = "Log in", command= face_validate)
+btn3 = Button (root, text="Exit", command = root.quit)
+btn.place(x = 500, y = 0)
+btn2.place(x= 0, y = 0)
+btn3.place(x=900, y = 0)
+
 
 #Database Connection:
-conn = pyodbc.connect()
+#conn = pyodbc.connect()
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor (frame, cv2.COLOR_BGR2GRAY)
+    faces = face_casecade.detectMultiScale(gray, 1.3,5)
+    for(x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+    cv2.imshow('Facial Recognition', frame)
+    if cv2.waitKey(1)& 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
 
 #Methods defined:
 def facial_collection():
@@ -39,11 +52,13 @@ def user_creation():
 
 
 def face_validate():
-    pass
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2Gray)
+    faces = face_casecade.detectMultiScale(gray, 1.3, 5)
 
 def finger_validate():
     pass
 
 #If Statements for each Button pushed:
+
 
 root.mainloop()
