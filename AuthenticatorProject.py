a536bd36b1af86e99ba429c4d5a3cc8d49d77df1
BranMerx <bmercadofranco@oakland.edu>
2024-11-12 13:59:28 -0500

    Registration Page

--- AuthenticatorProject.py
+++ AuthenticatorProject.py
@@ -8,6 +8,7 @@ import skimage as ski
 import pyodbc
 from tkinter import *
 from tkinter.ttk import *
+import threading
 
 face_casecade = cv2.CascadeClassifier(cv2.data.haarcascades + 'harcascade_frontalface_default.xml')
 finger_casecade = cv2.CascadeClassifier(cv2.data.haarcascades + 'harcascade_finger.xml')
@@ -24,49 +25,46 @@ btn3.place(x=900, y = 0)
 
 
 #Database Connection:
-server = ''
-database = ''
+server = 'MERX_LAPT\SQLEXPRESS'
+database = 'Authentication'
 username = ''
 password = ''
 
-connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={};database={};UID={};PWD={}'
+connection_string = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={'MERX_LAPT\SQLEXPRESS'};DATABASE={'Authentication'};UID={};PWD={}'
 conn = pyodbc.connect(connection_string)
 
 cursor = conn.cursor()
 
-cap = cv2.VideoCapture(0)
-while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor (frame, cv2.COLOR_BGR2GRAY)
-    faces = face_casecade.detectMultiScale(gray, 1.3,5)
-    for(x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
-    cv2.imshow('Facial Recognition', frame)
-    if cv2.waitKey(1)& 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
-
 #Methods defined:
+def user_creation():
+    username = input("Please Set Up Username: ")
+    password = input("Please Set Up Password: ")
+
+    facial_collection()
+    finger_collection()
+
 def facial_collection():
     pass
 
 def finger_collection():
     pass
 
-
-def user_creation():
-    pass
-
-
 def face_validate():
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2Gray)
-    faces = face_casecade.detectMultiScale(gray, 1.3, 5)
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor (frame, cv2.COLOR_BGR2GRAY)
+        faces = face_casecade.detectMultiScale(gray, 1.3,5)
+        for(x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255,0,0), 2)
+        cv2.imshow('Facial Recognition', frame)
+        if cv2.waitKey(1)& 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
 def finger_validate():
     pass
-
-#If Statements for each Button pushed:
 
 
 root.mainloop()
